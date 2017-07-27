8deb7a1a754b5730907242e6275599172cedfc2e
Pierce <pierceh.dev@gmail.com>
2017-07-27 10:11:06 -0500

    add pages home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   devise_for :users
+
   get   "/saml/auth",     to: "saml_idp#new"
   get   "/saml/metadata", to: "saml_idp#show"
   post  "/saml/auth",     to: "saml_idp#create"
   match "/saml/logout",   to: "saml_idp#logout", via: [:get, :post, :delete]
+
+  root to: "pages#home"
 end
