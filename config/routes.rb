21672cb0e76e4eb4a9ca3ef312c7bfd4c3933abe
Pierce <pierceh.dev@gmail.com>
2017-07-27 10:05:42 -0500

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get   "/saml/auth",     to: "saml_idp#new"
   get   "/saml/metadata", to: "saml_idp#show"
   post  "/saml/auth",     to: "saml_idp#create"
